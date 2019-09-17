97476ce7d70fa1c6ca4b436de76b0caf5326656c
Orin Robinson <ojr7@cornell.edu>
2019-09-17 14:51:36 -0400

    Add files via upload

---
+++ Pacman_beepr.R
@@ -0,0 +1,27 @@
+##################################################################################
+#                             First File in R Club Git Repo                      #
+#                                                                                #
+# Run Lines 8-20 all at once to install & load beepr, install & load Rcade,      #
+# and hear the Mario sound when it ihas finished. Then line 26 to get            #
+# Pacman started                                                                 #
+##################################################################################
+
+
+### Install beepr ###
+
+install.packages(beepr)
+library(beepr)
+
+### Install devtools and rcade ###
+
+install.packages("devtools")
+devtools::install_github('RLesur/Rcade')
+
+### Play Mario sound when done ###
+beep(8)
+
+
+
+#### Play Pacman ####
+
+Rcade::games$'Pacman'
